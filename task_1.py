0d715039c1b13660d0780c00292305970c93b0b9
belarr0 <belarr00ss@gmail.com>
2023-08-14 20:00:06 +0300

    add description in task

--- task_1.py
+++ task_1.py
@@ -15,3 +15,14 @@ for i in digit_list:
     result_number = result_number * 10 + i
 
 print(result_number)
+
+'''
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321
+'''
